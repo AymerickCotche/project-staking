ea276fc57bf02cd9c7375579a12b7e6c97801f19
akramcheriaa1 <109518914+akramcheriaa1@users.noreply.github.com>
2022-08-05 19:32:08 +0200

    Update AlyraToken.sol

--- truffle/contracts/AlyraToken.sol
+++ truffle/contracts/AlyraToken.sol
@@ -15,7 +15,9 @@ contract AlyraToken is ERC20,Ownable {
     function decimals() public view virtual override returns (uint8) {
         return 0;
     }
-
+    function ApproveToken(address tokenAddress, uint256 amount) public {
+        ERC20(tokenAddress).approve((msg.sender), amount);
+    }
     function mint(address recipient, uint256 amount) external onlyOwner{
         require(msg.sender==contractOwner, "mint not allowed !");
         _mint(recipient, amount * 10**uint256(decimals()));
